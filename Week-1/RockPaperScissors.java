b56d96518b61a513b4e091384b6c91674bd52bcd
mugeshkrishnaa <mugeshkrishnaa@gmail.com>
2026-09-22 10:49:33 +0530

    Create RockPaperScissors.java

---
+++ Week-1/RockPaperScissors.java
@@ -0,0 +1,47 @@
+package week1.class_problems;
+
+import java.util.*;
+
+public class RockPaperScissors {
+
+    static String playRound(String player, String computer) {
+        if (player.equals(computer))
+            return "Draw";
+
+        if ((player.equals("Rock") && computer.equals("Scissors")) ||
+            (player.equals("Paper") && computer.equals("Rock")) ||
+            (player.equals("Scissors") && computer.equals("Paper")))
+            return "Player Wins";
+
+        return "Computer Wins";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] moves = {"Rock", "Paper", "Scissors"};
+
+        int wins = 0, losses = 0, draws = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter Rock/Paper/Scissors: ");
+            String player = sc.next();
+
+            String computer = moves[(int)(Math.random() * 3)];
+            String result = playRound(player, computer);
+
+            System.out.println("Computer: " + computer);
+            System.out.println("Result: " + result);
+
+            if (result.equals("Player Wins")) wins++;
+            else if (result.equals("Computer Wins")) losses++;
+            else draws++;
+        }
+
+        double percentage = wins * 100.0 / 5;
+
+        System.out.println("\nWins: " + wins);
+        System.out.println("Losses: " + losses);
+        System.out.println("Draws: " + draws);
+        System.out.println("Win Percentage: " + percentage + "%");
+    }
+}
